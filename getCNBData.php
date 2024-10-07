50acd5bcd3333ba6b17fe159c3e11cb6cc9e478f
VitezslavDaricek <52070420+VitezslavDaricek@users.noreply.github.com>
2024-10-07 21:37:22 +0200

    Update getCNBData.php

--- getCNBData.php
+++ getCNBData.php
@@ -1,4 +1,9 @@
 <?php
+// Povolení CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 // URL ke kurzovnímu lístku ČNB
 $url = 'https://www.cnb.cz/en/financial_markets/foreign_exchange_market/exchange_rate_fixing/daily.txt';
 
